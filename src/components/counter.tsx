e33f6f4ea6e28e8e0453d303c68730397dbfdff3
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-10-30 10:53:29 +0100

    push

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,6 +1,7 @@
 import { ControlCounter } from "./control-counter";
 import { DisplayCounter } from "./display-counter";
 import { useControls } from "./hooks/useControls";
+import { Wrapper } from "./wrapper";
 
 export function Counter(){
 
@@ -8,13 +9,19 @@ export function Counter(){
 
     return (
         <>  
+        <Wrapper>
         <DisplayCounter
         value={controls.state.counter}
         />
+        </Wrapper>
+
+        <Wrapper>
 
         <ControlCounter
         actions={controls.actions}
         />
+        </Wrapper>
+
         </>
     )
 
